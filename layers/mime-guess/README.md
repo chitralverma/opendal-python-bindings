bb2a972baf1337cdee03ed67a6d779061f5b1029
chitralverma <chitralverma@gmail.com>
2026-02-04 19:03:37 +0530

    add mime-guess layer

---
+++ layers/mime-guess/README.md
@@ -0,0 +1,10 @@
+# Apache OpenDAL™ Python Bindings (mime-guess layer)
+
+![](https://img.shields.io/pypi/v/opendal-layer-mime-guess.svg?style=flat-square)
+![](https://img.shields.io/pypi/dm/opendal-layer-mime-guess.svg?style=flat-square)
+
+## Installation
+
+```bash
+pip install opendal-layer-mime-guess
+```
---
+++ layers/mime-guess/src/lib.rs
@@ -0,0 +1,41 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// expose the pyo3-opendal as `opyo3`.
+// We will use `opyo3::Xxx` to represents all types from pyo3-opendal.
+use ::pyo3_opendal as opyo3;
+use pyo3::prelude::*;
+
+use pyo3_stub_gen::define_stub_info_gatherer;
+
+#[allow(deprecated)]
+mod mime_guess;
+pub use mime_guess::*;
+
+#[pymodule(gil_used = false)]
+fn _layer_mime_guess(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    opyo3::check_debug_build!(py, env!("CARGO_PKG_NAME"))?;
+
+    // Add version
+    opyo3::add_version!(m)?;
+
+    m.add_class::<PyMimeGuessLayer>()?;
+
+    Ok(())
+}
+
+define_stub_info_gatherer!(stub_info);
